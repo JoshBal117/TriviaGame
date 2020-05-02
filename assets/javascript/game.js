a4c148a7edd7a90cfd6296a1a2026e009e147906
Joshua Balao <joshuacbalao@gmail.com>
2020-05-02 08:38:49 -0400

    add background image for game, add also start button for game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,20 @@ $(document).ready(function() {
     //on the document its self. 
    
     // setting up some variables, and an object within the vriable for the questions
+    let player = {
+        isPlaying: false,
+        isWaiting: false,
+        correctAnswers: 0,
+        incorrectAnswers: 0,
+        missedAnswer: 0,
+        wins: 0,
+        losses: 0,
+    }
     let trivQuestions = [] // thisis going to be an array of questions held here
     let score = 0;
+    let gameStatus = [];
+    let currentQuestion = 0;
     
-    let currentQuestion = 0 
-    let correctAnswers = 0; // this is going to equal the amount of right questions
-    let incorrectAnswers = 0;
-    let missedAnswer= 0;
     //this is the timer for the game
     let timer = 60;
     let timeOut = 1000 * 5;
@@ -27,9 +34,18 @@ startGame();
 
 
     function startGame(){
-        console.log('game is ready')
-        $('#startGame').on('click', )
-
+        $('#startGame').on('click', function(playerInitilization) {
+            player.isPlaying = !player.isPlaying
+            gameStatus= []
+            player.correctAnswers = 0;
+            player.incorrectAnswers = 0;
+            trivQuestions = createTrivQuestions();
+        })
+        console.log('startGame is ready')
+        
+        
+        
+        
         //this is  for loop to cycle through the questions
     
 
@@ -38,23 +54,23 @@ startGame();
         //this is the timer function for the game, if the player runs out of time for the whoe game
         //the game ends 
 
-        let interval = setInterval(function() {
-            timer--;
-            console.log('timer')
-            $('timer').text(timer);
-            if (timer === 0) {
-                currentQuestion++;
-                $("#trivbuttons").empty();
-                i++;
-                $("#response").empty();
-                logic();
-                timer = 60;
-        }
-        }, 1000);
+        // let interval = setInterval(function() {
+        //     timer--;
+        //     console.log('timer')
+        //     $('timer').text(timer);
+        //     if (timer === 0) {
+        //         currentQuestion++;
+        //         $("#trivbuttons").empty();
+        //         i++;
+        //         $("#response").empty();
+        //         logic();
+        //         timer = 60;
+        // }
+        // }, 1000);
 
 
-        function logic() {
+        // function logic() {
 
-        }
+        // }
     
 }})   
