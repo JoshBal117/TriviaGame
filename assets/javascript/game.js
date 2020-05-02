5e236a017291a87dd9489121226b7d7cebada9f8
Joshua Balao <joshuacbalao@gmail.com>
2020-05-01 20:28:33 -0400

    add more variables for score, correctAnswers, incorrect ones and timer

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    console.log(ready)
+
+
+    //these are the event listeners for the UI; having a timer, also having two onclick listeners 
+    //on the document its self. 
+    $('#remaining-time').show();
+    $('document').on('click','.option', trivia.startGame );
+    $('document').on('click', '.option', triva.guesschecker);
+    // setting up some variables, and an object within the vriable for the questions
+    let questions = [] // thisis going to be an array of questions held here
+    let score = 0; // this is going to equal the amount of right questions
+    let correctAnswers = 0;
+    let incorrectAnswers = 0;
+    let timer = 20;
+
+
+
+    //this is  for loop to cycle through the questions
+
+})
