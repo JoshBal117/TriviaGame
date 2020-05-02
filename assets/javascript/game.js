48305b3987b2ad5745f8744019b59e720da5c635
Joshua Balao <joshuacbalao@gmail.com>
2020-05-02 00:40:16 -0400

    add gamestart function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-    console.log(ready)
+    console.log("ready")
 
 
     //these are the event listeners for the UI; having a timer, also having two onclick listeners 
     //on the document its self. 
-    $('#remaining-time').show();
-    $('document').on('click','.option', trivia.startGame );
-    $('document').on('click', '.option', triva.guesschecker);
+   
     // setting up some variables, and an object within the vriable for the questions
-    let questions = [] // thisis going to be an array of questions held here
+    let trivia = [] // thisis going to be an array of questions held here
     let score = 0; // this is going to equal the amount of right questions
     let correctAnswers = 0;
     let incorrectAnswers = 0;
@@ -19,40 +17,34 @@ $(document).ready(function() {
     let timedOut = false;
     let isCorrect = false;
     let inervalId;
-    let timerRunning = false;
+    let timerRunning = false
 
 
-w
-    // this is the game start button
-    $('startGame').click(function()) {
-        //putting into place an object Interval so that we can stop the interval later
-        intervalId = setInterval(decrement, 1000)
-        //This is a JQuery fucntion to write the questions in place of the  Start Button 
-        writeQuestions();
-        $("startGame").hide();
-        writeSubmitButton();
-        console.log(writeSubmitButton "we are ready here")
+startGame();
 
 
+    function startGame(){
+        $('#remaining-time').show();
+        $('document').on('click','.option', trivia.startGame );
+        $('document').on('click', '.option', trivia.guesschecker);
 
-    }
-
-    //this is  for loop to cycle through the questions
+        //this is  for loop to cycle through the questions
 
 
 
-    //ths is the timer for countdown
-    // timer 
-    let timer = {
-        // Amount of time for each question
-        timeRemaining: timeSetting,
+        //ths is the timer for countdown
+        // timer 
+        let counter = {
+            // Amount of time for each question
+            timeRemaining: timeSetting,
 
         start: function() {
-
+            console.lot('ready')
             // Sets the interval if timer is not running
             if (!timerRunning) {
                 intervalId = setInterval(timer.count, 1000);
                 timerRunning = true;
+                console.log('timerRunning', 'FIRE!')
             }
         },
 
@@ -75,10 +67,10 @@ w
                 timedOut = true;
                 timer.stop();
                 outOfTime();
-            } console.log(timerRemaining)
+            } console.log('timerRemaining')
 
     }
 
-};
-}
-    
+        }; 
+    }
+})   
