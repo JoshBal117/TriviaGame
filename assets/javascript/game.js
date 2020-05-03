92b4fee2ae66c94813eb8584068b8e7ad22ec137
Joshua Balao <joshuacbalao@gmail.com>
2020-05-03 19:55:57 -0400

    corrected undefined value and made it so questions appeared!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,53 +8,31 @@ $(document).ready(function() {
     // setting up some variables, and an object within the vriable for the questions
   
     
-    let trivQuestions = [
-        {
-            Question : "Yoda is how old in the Return of The Jedi?",
-        Answers:[ 
-                 "700 years old", 
-                 "800 years old", 
-                 "900 years old", 
-                 "1100 years old",
-        ],
+    let trivQuestions = [{
+            Question: "Yoda is how old in the Return of The Jedi?",
+            Answers: ["700 years old" , "800 years old" , "900 years old" , "1100 years old"],
             correctAnswer: "3"
-        },
-        {Question : "Luke wanted to get what from Tosche Station?",
-        Answers:[   
-                 "Power Couplings", 
-                 "Power Converters", 
-                 "Droid Inhibitor", 
-                 "Lightsaber"
-        ],
+    },
+    {
+            Question: "Luke wanted to get what from Tosche Station?",
+            Answers:["Power Couplings" , "Power Converters" , "Droid Inhibitor" , "Lightsaber"],
             correctAnswer: "2"
-        },
-           {Question: "Anakin is what as a child?",
-           Answers:[
-                 "A Jedi", 
-                 "A Store Owner", 
-                 "A Pilot", 
-                 "A Slave",
-    ],
+    },
+    {
+            Question: "Anakin is what as a child?",
+           Answers:["A Jedi" , "A Store Owner" , "A Pilot" , "A Slave"],
            correctAnswer: "4"
-        },
-        {Question: "Obi Wan Kenobi is called what by Princess Leia?",
-            Answers:[
-                 "An Old Goat", 
-                 "General Kenobi", 
-                 "Ben Kenobi", 
-                 "The Negotiator"
-    ],
+    },
+    {
+            Question: "Obi Wan Kenobi is called what by Princess Leia?",
+            Answers:["An Old Goat" , "General Kenobi" , "Ben Kenobi",  "The Negotiator"],
             correctAnswer: "2"
-        },
-            {Question: "Who is pursuing the Rebel Blockade Runner in Episode IV?",
-            Answers:[
-                 "Darth Vader", 
-                 "Darth Maul", 
-                 "Darth Tyranus", 
-                 "Darth Bane" 
-            ],
+    },
+    {
+            Question: "Who is pursuing the Rebel Blockade Runner in Episode IV?",
+            Answers:["Darth Vader" ,  "Darth Maul" ,  "Darth Tyranus" ,  "Darth Bane"],
             correctAnswer: "1"            
-                    },
+    },
     ] // this is going to be an array of questions held here
     let score = 0;
     let gameStatus = [];
@@ -99,44 +77,26 @@ $(document).ready(function() {
                 userPick[this.name] = this.value;
             });
         });
-    
-        function answerButton() {
-            for (var i = 0; i < trivQuestions[currentQuestion].Answers.length; i++) {
-        
-                // We create a button 
-                var button = $("<button>");
-        
-                // Assigns the name of the array to the button.
-                button.text(trivQuestion[currentQuestion].Answers[i]);
-        
-                // We create a class for the buttons 
-                button.addClass("answer-buttons btn btn-primary");
-        
-                //ADDED AN ATTRIBUTE TO THE BUTTON THAT CREATES A PATH TO THE VALUES ARRAY AND TIES THE VALUE TO THE BUTTON. EXAMPLE. THE INDEX[0] OF VALUES WILL BE TIED TO THE INDEX[0] OF CHOICES.
-                button.attr("value", trivQuestion[currentQuestion].Answers[i]);
-                // CREATES AN ATTRIBUTE DATA NAME AND SETS IT EQUAL TO THE ARRAY ITEMS NAME.
-                button.attr("Data-name", trivQuestion[currentQuestion].Answers[i]);
-                // WE APPEND the button in the div with the #trivButtons id.
-                $("#trivButtons").append(button);
-            };
-        }
 
         function writeQuestions() {
             for (var i = 0; i < trivQuestions.length; i++) {
-                $("#formQuiz").append(trivQuestions[i].question + "</br>");
-                console.log()
+                $("#formQuiz").append(trivQuestions[i].Question + "</br>");
+            
                 //From within the first loop, write out the radio option buttons and assign them values and names of x and i respectively for later evaluation
                 for (var x = 0; x < trivQuestions[i].Answers.length; x++) {
                     $("#formQuiz").append("<label class='radio-inline'><input value='" + x + "' type='radio' name='" + i + "'>" + trivQuestions[i].Answers[x] + "</label>");
-                console.log("var")
+                
                 }
                 $("#formQuiz").append("<br/><br/>");
             }
         }
-        //Write the button to submit the form in the event the user does not want to wait for the timer expire event.
-        function writeSubmitButton() {
-            $("#formSubmit").append("<button id='submitQuiz' class='btn btn-primary btn-lg'>Submit</button>");
-        }
+    
+            //Write the button to submit the form in the event the user does not want to wait for the timer expire event.
+            function writeSubmitButton() {
+    $("#formSubmit").append("<button id='submitQuiz' class='btn btn-primary btn-lg'>Submit</button>");
+    }
+   
+        
     //Countdown counter
 function decrement() {
     counter--;
